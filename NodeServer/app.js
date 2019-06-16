889ef8a937018fa36bc6c20edfcecc921a4376b6
asfedez <solanofedezadrian@gmail.com>
2019-06-16 15:29:07 -0600

    NodeServer
    Configuración del servidor

---
+++ NodeServer/app.js
@@ -0,0 +1,85 @@
+const Express = require('express');
+const BodyParser = require('body-parser');
+const Mysql = require('mysql');
+const cors = require('cors');
+
+const app = Express();
+
+/* Enable Cors */
+app.use(cors());
+
+
+/* Express Server Config */
+app.set('port', process.env.port || 3000);
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true}));
+
+
+/* Database Configuration */
+const connection = Mysql.createConnection({
+    hostname : 'localhost:8100',
+    user     : 'root',
+    password : '',
+    database : 'multimediosdb'
+});
+
+connection.connect( function(err) {
+    if(err){
+        console.log(err);
+    }else{
+        console.log('conectado');
+    }
+});
+
+
+
+/* Model */
+
+
+
+
+/* Routes */
+app.get("/api/incomes", (request, response) =>{
+    try {
+        connection.query('SELECT * FROM `income`', function (error, results, fields) {
+            if (error) throw error;
+            return response.send({results});
+        });
+
+    } catch (error) {
+        response.status(500).send(error);
+    }
+})
+
+app.get("/api/incomes/:id", (request, response) =>{
+    try {
+        let {id} = request.params;
+        connection.query('SELECT * FROM income where id=?',id ,function (error, results, fields) {
+            if (error) throw error;
+            return response.send({results});
+        });
+    } catch (error) {
+        response.status(500).send(error);
+    }
+})
+
+
+app.post("/api/incomes", (request, response) =>{
+    try {
+        let {id} = request.params;
+        connection.query('SELECT * FROM income where id=?',id ,function (error, results, fields) {
+            if (error) throw error;
+            return response.send({results});
+        });
+
+        
+    } catch (error) {
+        response.status(500).send(error);
+    }
+})
+
+
+
+app.listen(app.get('port'), ()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
