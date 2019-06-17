7e5bb4085a8ecb3808fc52f4806cc9dfd12d6c53
asfedez <solanofedezadrian@gmail.com>
2019-06-16 18:09:02 -0600

    Componentes

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { IncomesComponent } from './components/incomes/incomes.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+import { SavingsComponent } from './components/savings/savings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    IncomesComponent,
+    ExpensesComponent,
+    SavingsComponent
   ],
   imports: [
     BrowserModule,
